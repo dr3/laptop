1c2281d8ab2afc8ec4423c22977f6fb4e17032d9
Drew McMillan <drew@mcmill.co.uk>
2023-09-19 23:04:41 +0100

    fix iterm profile

--- sources/iterm.sh
+++ sources/iterm.sh
@@ -1,6 +1,6 @@
 #
 # Instals my iTerm2 profile
 #
-curl -s ~/dev/laptop/files/iterm-dynamic.json -o ~/Library/Application\ Support/iTerm2/DynamicProfiles/drew.json
+cat ~/dev/laptop/files/iterm-dynamic.json > ~/Library/Application\ Support/iTerm2/DynamicProfiles/drew.json
 echo -e "\033]50;SetProfile=DrewDarkProfile\a"
 clear;
