f54ff52a2c74148a8480ea5efbba5ad7b0c5e29a
Drew McMillan <drew.mcmillan@thetrainline.com>
2024-01-17 15:28:17 +0000

    🔧 Continue to implement and debug

---
+++ scripts/mitm-reply-from-proxy.py
@@ -0,0 +1,13 @@
+# mitmweb --set web_port=8082 --ssl-insecure -s ~/dev/laptop/scripts/mitm-reply-from-proxy.py
+
+"""Send a reply from the proxy without sending the request to the remote server."""
+from mitmproxy import http
+
+
+def request(flow: http.HTTPFlow) -> None:
+    if flow.request.pretty_url == "https://google.com/path":
+        flow.response = http.Response.make(
+            410,  # (optional) status code
+            b"{}",  # (optional) content
+            {"Content-Type": "application/json"},  # (optional) headers
+        )
--- sources/misc-path.sh
+++ sources/misc-path.sh
@@ -17,6 +17,7 @@ export PATH=/opt/homebrew/bin:$PATH
 export PATH=~/bin:$PATH
 export PATH="$HOME/.serverless/bin:$PATH"
 export PATH=$(pyenv root)/shims:$PATH
+export PATH="/opt/homebrew/opt/curl/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
