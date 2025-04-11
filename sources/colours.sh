175a9e98890cf3a5f72dedb9145bf0058132fa93
Drew McMillan <drew.mcmillan@thetrainline.com>
2025-04-11 14:02:20 +0100

    tweak

--- sources/colours.sh
+++ sources/colours.sh
@@ -1,7 +1,6 @@
 #
-# Set VScode colours
+# Set VScode & Iterm colours
 #
-
 light() {
   sed -i -e 's/"workbench.colorTheme": ".*"/"workbench.colorTheme": "Default Light+"/g' "$HOME/Library/Application Support/Code/User/settings.json";
   lightIterm;
@@ -15,24 +14,15 @@ dark() {
 #
 # Instals my iTerm2 profile
 #
-
 cat ~/dev/laptop/files/iterm-dynamic.json > ~/Library/Application\ Support/iTerm2/DynamicProfiles/drew.json
-
-lightIterm() {
-  echo -e "\033]50;SetProfile=DrewLightProfile\a"
-  clear;
-}
-
-darkIterm() {
-  echo -e "\033]50;SetProfile=DrewDarkProfile\a"
-  clear;
-}
 
 #
 # Set iTerm2 profile based on VScode theme
 #
 if grep -q Default Light+ "$HOME/Library/Application Support/Code/User/settings.json"; then
-  lightIterm;
+  echo -e "\033]50;SetProfile=DrewLightProfile\a"
+  clear;
 else
-  darkIterm;
+  echo -e "\033]50;SetProfile=DrewDarkProfile\a"
+  clear;
 fi
